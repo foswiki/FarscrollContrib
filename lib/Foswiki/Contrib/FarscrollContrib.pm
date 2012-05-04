c025e28d7bf8a8ca465f8f477958140446031439
OlivierRaginel <OlivierRaginel@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2012-05-04 13:27:49 +0000

    Item11808: perltidy them all, so people are not accidentally impacted by the new enforced rule -- Sorry Micha, could not find a way to define specific values within the file
    git-svn-id: http://svn.foswiki.org/trunk/FarscrollContrib@14686 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/FarscrollContrib.pm
+++ lib/Foswiki/Contrib/FarscrollContrib.pm
@@ -3,14 +3,15 @@ package Foswiki::Contrib::FarscrollContrib;
 use strict;
 use warnings;
 
-our $VERSION = '$Rev$';
-our $RELEASE = '1.0.0';
-our $SHORTDESCRIPTION = 'Clone a scrollbar far away';
+our $VERSION           = '$Rev$';
+our $RELEASE           = '1.0.0';
+our $SHORTDESCRIPTION  = 'Clone a scrollbar far away';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub init {
-  require Foswiki::Plugins::JQueryPlugin ();
-  Foswiki::Plugins::JQueryPlugin::registerPlugin("Farscroll", "Foswiki::Contrib::FarscrollContrib::Core");
+    require Foswiki::Plugins::JQueryPlugin();
+    Foswiki::Plugins::JQueryPlugin::registerPlugin( "Farscroll",
+        "Foswiki::Contrib::FarscrollContrib::Core" );
 }
 
 1;
--- lib/Foswiki/Contrib/FarscrollContrib/Core.pm
+++ lib/Foswiki/Contrib/FarscrollContrib/Core.pm
@@ -7,22 +7,22 @@ use Foswiki::Plugins::JQueryPlugin::Plugin ();
 our @ISA = qw( Foswiki::Plugins::JQueryPlugin::Plugin );
 
 sub new {
-  my $class = shift;
+    my $class = shift;
 
-  my $this = bless(
-    $class->SUPER::new(
-      name => 'Farscroll',
-      version => '0.1.0',
-      author => 'Sebastian Senf',
-      homepage => 'https://github.com/mustardamus/farscroll-plugin',
-      javascript => ['jquery.farscroll.js'],
-      documentation => 'FarscrollContrib',
-      puburl => '%PUBURLPATH%/%SYSTEMWEB%/FarscrollContrib',
-    ),
-    $class
-  );
+    my $this = bless(
+        $class->SUPER::new(
+            name          => 'Farscroll',
+            version       => '0.1.0',
+            author        => 'Sebastian Senf',
+            homepage      => 'https://github.com/mustardamus/farscroll-plugin',
+            javascript    => ['jquery.farscroll.js'],
+            documentation => 'FarscrollContrib',
+            puburl        => '%PUBURLPATH%/%SYSTEMWEB%/FarscrollContrib',
+        ),
+        $class
+    );
 
-  return $this;
+    return $this;
 }
 
 1;
