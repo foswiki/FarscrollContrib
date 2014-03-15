7ea73df7cc412b910ed589a87e8e7ea30e809f5f
MichaelDaum <MichaelDaum@0b4bb1d4-4e5a-0410-9cc4-b2b747904278>
2014-03-15 09:10:45 +0000

    Item12784: fixed syntax in version stub
    git-svn-id: http://svn.foswiki.org/trunk/FarscrollContrib@17346 0b4bb1d4-4e5a-0410-9cc4-b2b747904278

--- lib/Foswiki/Contrib/FarscrollContrib.pm
+++ lib/Foswiki/Contrib/FarscrollContrib.pm
@@ -3,13 +3,13 @@ package Foswiki::Contrib::FarscrollContrib;
 use strict;
 use warnings;
 
-our $VERSION = '$Rev$';
-our $RELEASE = '1.0.0';
+our $VERSION = '1.01';
+our $RELEASE = '1.01';
 our $SHORTDESCRIPTION = 'Clone a scrollbar far away';
 our $NO_PREFS_IN_TOPIC = 1;
 
 sub init {
-  require Foswiki::Plugins::JQueryPlugin ();
+  require Foswiki::Plugins::JQueryPlugin;
   Foswiki::Plugins::JQueryPlugin::registerPlugin("Farscroll", "Foswiki::Contrib::FarscrollContrib::Core");
 }
 
